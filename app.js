0c5abff12c8bcd3418fa463257880c6360016f85
Emiliano Gusberti <emggw@hotmail.com>
2022-12-26 19:18:34 -0300

    Removed res.send(html), added API call

--- app.js
+++ app.js
@@ -17,9 +17,31 @@ app.post("/", function (req, res) {
   var lastName = req.body.lastName;
   var email = req.body.email;
 
-  res.send(`<h1>Thanks for suscribing, ${firstName}!</h1>
-  <p>Every week we'll send our news to: ${email}!</p>`);
+  const client = require(process.env.EMAIL_ROOT_URL);
+
+  client.setConfig({
+    apiKey: process.env.EMAIL_API_KEY,
+    server: process.env.EMAIL_SERVER_ID,
+  });
+
+  const runApiCall = async () => {
+    const response = await client.lists.addListMember(
+      process.env.EMAIL_LIST_ID,
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      }
+    );
+  };
+
+  runApiCall();
 });
+
+const test = process.env.EMAIL_API_KEY;
 
 const port = 3000;
 app.listen(port, function () {
